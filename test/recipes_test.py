fb7582ffd317017887c4ca0635a90a4c67c6c783
brigittewebb <bgw17@georgetown.edu>
2024-12-16 23:20:12 -0500

    Website formatting (#6)
    * Additional testing
    
    * Testing edits
    
    * Website formatting

--- test/recipes_test.py
+++ test/recipes_test.py
@@ -1,6 +1,6 @@
 # this is the "test/recipes_test.py" file...
 
-from app.recipes import retrieve_recipes_by_ingredients
+from app.recipes import retrieve_recipes_by_ingredients, retrieve_recipes_by_keyword, retrieve_recipe_info
 
 def test_data_fetching_by_ingredients():
     
@@ -17,3 +17,23 @@ def test_data_fetching_by_ingredients():
     data = retrieve_recipes_by_ingredients(selected_search_criteria,selected_cuisine, selected_diet, selected_intolerances, selected_dish_type, selected_maxReadyTime, selected_sort, selected_number, selected_fillIngredients)
     assert isinstance(data, list)
     assert len(data) == selected_number
+
+def test_data_fetching_by_keyword():
+    
+    selected_search_criteria = "banana bread"
+    selected_diet = None # Special diets (ex., "vegetarian")--dropdown list
+    selected_intolerances = None # Dietary resrictions (ex., "gluten")--dropdown list
+    selected_number = 1 # Number of desired results
+    selected_fillIngredients = True
+    
+    data = retrieve_recipes_by_keyword(selected_search_criteria, selected_diet, selected_intolerances, selected_number, selected_fillIngredients)
+    assert isinstance(data, list)
+    assert len(data) == selected_number
+
+def test_data_fetching_by_recipe_id():
+
+    recipe_id = 646541
+
+    data = retrieve_recipe_info(recipe_id)
+    assert isinstance(data, dict)
+    assert data["title"] == "Heart Healthy, Whole-Grain Brownies"
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,16 +5,13 @@
 import os
 from flask import Flask
 
-#from web_app.routes.log_in_routes_routes import log_in_routes
 from web_app.routes.recipes_routes import recipes_routes # Need to do this for each routes file
 
-### What should we set for our secret key? ###
-SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+SECRET_KEY = os.getenv("SECRET_KEY", default="dkjwleijowijlksdaqwanbbdskne")
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY
-    #app.register_blueprint(log_in_routes)
     app.register_blueprint(recipes_routes) # Need to do this for each routes file
     return app
 
